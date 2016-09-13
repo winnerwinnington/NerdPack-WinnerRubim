3c99f8eacbae939afde05684fac8de5c860e2844
jrubimf <jrubimf@gmail.com>
2016-09-13 01:06:34 -0300

    Add files via upload

--- rotations/dk/blood.lua
+++ rotations/dk/blood.lua
@@ -1,38 +1,38 @@
 local exeOnLoad = function()
 --	NePCR.Splash()
-	meeleSpell = 49998
-	print("Meele Spell: ".. GetSpellInfo(meeleSpell) .. "(" .. meeleSpell .. ")")
+--m	meeleSpell = 49998
+	print("|cffFFFF00 ----------------------------------------------------------------------|r")
+	print("|cffFFFF00 --- |rDeath Knight |cffC41F3BBlood |r")
+	print("|cffFFFF00 --- |rRecommended Talents: 1/2 - 2/1 - 3/1 - 4/2 - 5/1 - 6/3 - 7/1")
+	print("|cffFFFF00 --- |rDnD usage is not optimal. Read the Readme avaiable at github.")
+	print("|cffFFFF00 ----------------------------------------------------------------------|r")
 
 	NeP.Interface.CreateToggle(
 		'saveDS',
 		'Interface\\Icons\\spell_deathknight_butcher2.png',
-		'Save a Death Strike',
-		'Saving Runic.')
+		'Save Death Strike',
+		'BOT will Only Death Strike when RP is Capped, useful on fights were you need to cast an active mitigation.')
 		
 	NeP.Interface.CreateToggle(
 		'bonestorm',
 		'Interface\\Icons\\Ability_deathknight_boneshield.png',
 		'Use Bonestorm',
-		'Pool RP.')
+		'This will pool RP to use Bonestorm.')
 	
 	NeP.Interface.CreateToggle(
 		'aoetaunt',
 		'Interface\\Icons\\spell_nature_shamanrage.png',
 		'Aoe Taunt',
-		'Aoe Taunt.')	
+		'Experimental AoE Taunt.')	
 	
 end
 
 local DeathStrikeHELL = {
-	{ "Death Strike" , { "player.health <= 50" , "!toggle.saveDS" }}, --DS Emergency		
+	{ "Death Strike" , { "player.health <= 65" , "!toggle.saveDS" }}, --DS Emergency		
 	{{ --Bonestormless
 		{ "Death Strike" , { "player.health <= 85" , "!toggle.saveDS" }}, --DS to Heal
 		{ "Death Strike" , "player.runicpower >= 90" }, --DS for RP Dump		
 	}, "!talent(7, 1)"},
-	{{ --Bonestormless
-		{ "Death Strike" , { "player.health <= 85" , "!toggle.saveDS" }}, --DS to Heal
-		{ "Death Strike" , "player.runicpower >= 90" }, --DS for RP Dump		
-	}, "@Rubim.AreaTTD()"},
 	{{ --Bonestorm
 		{ "Death Strike" , { "player.health <= 85" , "!toggle.saveDS" , "!toggle.bonestorm" }}, --DS to Heal
 		{ "Death Strike" , { "player.health <= 85" , "!toggle.saveDS" , "toggle.bonestorm" , "player.spell(Bonestorm).cooldown >= 2" }}, --DS to Heal
@@ -42,12 +42,11 @@ local DeathStrikeHELL = {
 }
 
 local Shared = {
-	{'@Rubim.CastGroundSpell()'},
+--	{'@Rubim.CastGroundSpell()'},
 }
 
 local Survival = {
-	{ "Vampiric Blood" , { "player.runicpower >= 35" , "player.health <= 85" , "@Rubim.MeeleRange" }}, --VP with enough RP for a DS
-	{ '#5512', 'player.health < 70'}, --Healthstone
+	{ "Vampiric Blood" , { "player.runicpower >= 35" , "player.health <= 85" , "player.rubimarea(7).enemies >= 1" }}, --VP with enough RP for a DS
 }
 
 local Interrupts = {
@@ -61,44 +60,56 @@ local Cooldowns = {
 
 local Burst = {
 	{"Marrowrend" , "player.buff(Bone Shield).count <= 2" },
-	{"Heart Strike"},
-	{"Blood Boil" , "player.rarea(8).enemies >= 1" },
+	{{
+		{'Death Strike' , 'player.health <= 70' },
+		{'Death Strike' , 'player.runicpower >= 75'},
+		{'Blood Boil'},
+		{'Heart Strike' },	
+	}, 'player.rubimarea(7).enemies >= 2'},
+	
+	{{
+		{'Death Strike' , 'player.runicpower >= 75'},
+		{'Marrowrend'},
+		{'Death Strike'},
+		{'Blood Boil'},
+	}, 'player.rubimarea(7).enemies <= 1'},
 }
 
 local Util = {
 	--AUTOTARGET
 	{ '@Rubim.Targeting()' , '!target.alive' },
-	
+	{ "@Rubim.AoETaunt()" , "toggle.aoetaunt" },
 	--BOSS
 	{ '%pause' , 'player.debuff(200904)' },
 	{ '%pause' , 'player.debuff(Soul Saped)' },
+}
+
+local General = {
+	{{
+	{"Marrowrend" , "player.buff(Bone Shield).count <= 5" },
+	{"Heart Strike" , "player.buff(Bone Shield).count >= 6"},
+	{"Blood Boil" , { "player.spell(Blood Boil).charges >= 2" , "player.rubimarea(8).enemies >= 1" }},
+	}, "player.rubimarea(10).enemies <= 2" },
 	
-	--
+	{{
+	{"Marrowrend" , "player.buff(Bone Shield).count <= 2" },
+	{"Blood Boil" , "player.rubimarea(8).enemies >= 1"},
+	{"Heart Strike" },
+	}, "player.rubimarea(10).enemies >= 3" },
 }
 
 
 local inCombat = {
 	{ Util },
-	{ "@Rubim.AoETaunt()" , "toggle.aoetaunt" },
 	{ Interrupts, "target.interruptAt(10)" }, -- Interrupt when 40% into the cast time
-	{ Cooldowns , "player.rarea(8).enemies >= 1" },
+	{ Cooldowns , "player.rubimarea(8).enemies >= 1" },
+	{"Blood Boil", { "target.debuff(Blood Plague).duration < 1.5" , "player.rubimarea(8).enemies >= 1" }},
 	{ Survival },
-	{ "Bonestorm" , { "player.runicpower >= 90" , "toggle.bonestorm" }}, --Bonestorm with enough RP
+	{ Burst , "player.areattd <= 5"},
+	{ "Bonestorm" , { "player.runicpower >= 90" , "toggle.bonestorm" , "player.areattd >= 10" }}, --Bonestorm with enough RP
 	{ DeathStrikeHELL },
-	{"Blood Boil", { "target.debuff(Blood Plague).duration < 1.5" , "player.rarea(8).enemies >= 1" }},
-	{ Burst , "@Rubim.AreaTTD()"},
-	
-	{{
-	{"Marrowrend" , "player.buff(Bone Shield).count <= 5" },
-	{"Heart Strike" , "player.buff(Bone Shield).count >= 6"},
-	{"Blood Boil" , { "player.spell(Blood Boil).charges >= 2" , "player.rarea(8).enemies >= 1" }},
-	}, "player.rarea(10).enemies <= 2" },
-	
-	{{
-	{"Marrowrend" , "player.buff(Bone Shield).count <= 2" },
-	{"Blood Boil" , "player.rarea(8).enemies >= 1"},
-	{"Heart Strike" },
-	}, "player.rarea(10).enemies >= 3" },
+	{ "Death and Decay"},
+	{ General },
 }
 
 local outCombat = {
